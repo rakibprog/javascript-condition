c7d4e6e7705193729210849d07845c39bd8d02e2
rakibprog <contact.engrakib@gmail.com>
2022-05-15 11:28:41 +0600

    javascript condition

---
+++ javascript-condition.js
@@ -0,0 +1,33 @@
+// condition 
+
+var a = 100;
+var b = 200;
+if(a > b){
+    console.log('positive');
+}else{
+    console.log('not a positive');
+}
+
+
+// for loop 
+
+for(var i = 0; i <= 10 ; i++){
+    console.log(i);
+}
+
+// while loop 
+
+var i = 0;
+
+while(i <= 10){
+    console.log(i);
+    i++;
+}
+
+// // even and ood number
+
+var i = 0; 
+while(i < 10){
+    console.log(i)
+    i = i + 2;
+}
